673bb21c83cebdff331f1e3820d71331a40c259b
Nycolas Souza <nycolascanutto@gmail.com>
2026-06-28 03:50:45 +0100

    [refactor] translate firmware identifiers to English

--- IoTController/IoTController.ino
+++ IoTController/IoTController.ino
@@ -1,11 +1,11 @@
-#define LED_VERMELHO 10
-#define LED_VERDE 11
-#define BOTAO_VERMELHO 9
-#define BOTAO_VERDE 8
+#define RED_LED 10
+#define GREEN_LED 11
+#define RED_BUTTON 9
+#define GREEN_BUTTON 8
 #define BUZZER A0
 
-char *texto[] = {"Identidade Válida0", "Identidade Inválida1", "LED VERMELHO ON", "LED VERMELHO OFF", "LED VERDE ON", "LED VERDE OFF", "ALARME ON", "ALARME OFF"}; // Textos exibidos no terminal do PC
-String charsIn = ""; // Esse é o caractere lido pelo Arduino
+char *messages[] = {"Valid Identity0", "Invalid Identity1", "RED LED ON", "RED LED OFF", "GREEN LED ON", "GREEN LED OFF", "ALARM ON", "ALARM OFF"}; // Textos exibidos no terminal do PC
+String serialInput = ""; // Esse é o caractere lido pelo Arduino
 char printout[20]; // Ainda não entendi para que serve
 
 const int buzzerFrequency = 1000; // Frequência de 1000 Hz
@@ -13,10 +13,10 @@ const int buzzerFrequency = 1000; // Frequência de 1000 Hz
 void setup()
 {
   // Faço o setup das portas usadas
-  pinMode(LED_VERMELHO, OUTPUT);
-  pinMode(LED_VERDE, OUTPUT);
-  pinMode(BOTAO_VERMELHO, INPUT);
-  pinMode(BOTAO_VERDE, INPUT);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(RED_BUTTON, INPUT);
+  pinMode(GREEN_BUTTON, INPUT);
   pinMode(BUZZER, OUTPUT); // Configura a porta do buzzer como saída
 
   // Inicia a porta serial em 9600
@@ -24,34 +24,34 @@ void setup()
 
   // Mostra o comando de conexão estabelecida
   delay(250);
-  Serial.println("Conexão Estabelecida");
+  Serial.println("Connection Established");
 }
 
-void acender_botao(int conector, char *texto, bool ativar_buzzer)
+void activateButton(int pin, char *message, bool activateBuzzer)
 {
-  digitalWrite(conector, HIGH); // Ele ativa o led
-  if (ativar_buzzer)
+  digitalWrite(pin, HIGH); // Ele ativa o led
+  if (activateBuzzer)
   {
     tone(BUZZER, buzzerFrequency); // Ativa o buzzer com a frequência especificada
   }
-  Serial.println(texto);        // Ele envia a mensagem de confirmação junto do ID para salvar na tabela de eventos
+  Serial.println(message);      // Ele envia a mensagem de confirmação junto do ID para salvar na tabela de eventos
   delay(2000);                  // Tem um delay de 2s
-  digitalWrite(conector, LOW);  // Desliga o led
-  if (ativar_buzzer)
+  digitalWrite(pin, LOW);       // Desliga o led
+  if (activateBuzzer)
   {
     noTone(BUZZER); // Desativa o buzzer
   }
 }
 
-void interagir_led(int conector, char *texto, int estado)
+void controlLed(int pin, char *message, int state)
 {
-  digitalWrite(conector, estado); // Ele vai acender o led
-  Serial.println(texto);          // E vai mandar a mensagem de confirmação
+  digitalWrite(pin, state); // Ele vai acender o led
+  Serial.println(message);  // E vai mandar a mensagem de confirmação
 }
 
-void interagir_buzzer(char *texto, int estado)
+void controlBuzzer(char *message, int state)
 {
-  if (estado == HIGH)
+  if (state == HIGH)
   {
     tone(BUZZER, buzzerFrequency); // Ativa o buzzer com a frequência especificada
     delay(2000);                   // Mantém o buzzer ativado por 2s
@@ -61,12 +61,12 @@ void interagir_buzzer(char *texto, int estado)
   {
     noTone(BUZZER); // Desativa o buzzer
   }
-  Serial.println(texto); // E vai mandar a mensagem de confirmação
+  Serial.println(message); // E vai mandar a mensagem de confirmação
 }
 
-void interagir_buzzer_comando(char *texto, int estado)
+void controlBuzzerCommand(char *message, int state)
 {
-  if (estado == HIGH)
+  if (state == HIGH)
   {
     tone(BUZZER, buzzerFrequency); // Ativa o buzzer com a frequência especificada
   }
@@ -74,7 +74,7 @@ void interagir_buzzer_comando(char *texto, int estado)
   {
     noTone(BUZZER); // Desativa o buzzer
   }
-  Serial.println(texto); // E vai mandar a mensagem de confirmação
+  Serial.println(message); // E vai mandar a mensagem de confirmação
 }
 
 
@@ -83,66 +83,66 @@ void loop()
   while (Serial.available()) // Faz a leitura dos comandos dados ao PC
   {
     char charRead = Serial.read();
-    charsIn.concat(charRead);
+    serialInput.concat(charRead);
   }
 
   // Abaixo são os comandos de interação do Arduino (Veja o README para entender os comandos do Arduino)
 
-  if (digitalRead(BOTAO_VERDE)) // Se o botão do led verde for pressionado
+  if (digitalRead(GREEN_BUTTON)) // Se o botão do led verde for pressionado
   {
-    acender_botao(LED_VERDE, texto[0], false); // Vai acender o led verde por 2s e enviar a mensagem de confirmação junto com o ID do evento
+    activateButton(GREEN_LED, messages[0], false); // Vai acender o led verde por 2s e enviar a mensagem de confirmação junto com o ID do evento
   }
 
-  if (digitalRead(BOTAO_VERMELHO)) // Se o botão do led vermelho for pressionado
+  if (digitalRead(RED_BUTTON)) // Se o botão do led vermelho for pressionado
   {
-    acender_botao(LED_VERMELHO, texto[1], true); // Vai acender o led vermelho por 2s, ativar o buzzer e enviar a mensagem de confirmação junto com o ID do evento
+    activateButton(RED_LED, messages[1], true); // Vai acender o led vermelho por 2s, ativar o buzzer e enviar a mensagem de confirmação junto com o ID do evento
   }
 
   // Abaixo são os comandos controlados pelo PC (Veja o README para entender os comandos do PC)
 
-  if (charsIn != "") // Caso tenha alguma entrada de dados vinda do PC
+  if (serialInput != "") // Caso tenha alguma entrada de dados vinda do PC
   {
-    if (charsIn == "i") // Caso essa entrada seja o caractere i
+    if (serialInput == "i") // Caso essa entrada seja o caractere i
     {
-      interagir_led(LED_VERMELHO, texto[2], HIGH); // Vai acender o led vermelho e enviar a mensagem de confirmação
+      controlLed(RED_LED, messages[2], HIGH); // Vai acender o led vermelho e enviar a mensagem de confirmação
     }
 
-    if (charsIn == "o") // Caso essa entrada seja o caractere o
+    if (serialInput == "o") // Caso essa entrada seja o caractere o
     {
-      interagir_led(LED_VERMELHO, texto[3], LOW); // Vai apagar o led vermelho e enviar a mensagem de confirmação
+      controlLed(RED_LED, messages[3], LOW); // Vai apagar o led vermelho e enviar a mensagem de confirmação
     }
 
-    if (charsIn == "k") // Caso essa entrada seja o caractere k
+    if (serialInput == "k") // Caso essa entrada seja o caractere k
     {
-      interagir_led(LED_VERDE, texto[4], HIGH); // Vai acender o led verde e enviar a mensagem de confirmação
+      controlLed(GREEN_LED, messages[4], HIGH); // Vai acender o led verde e enviar a mensagem de confirmação
     }
 
-    if (charsIn == "l") // Caso essa entrada seja o caractere l
+    if (serialInput == "l") // Caso essa entrada seja o caractere l
     {
-      interagir_led(LED_VERDE, texto[5], LOW); // Vai apagar o led verde e enviar a mensagem de confirmação
+      controlLed(GREEN_LED, messages[5], LOW); // Vai apagar o led verde e enviar a mensagem de confirmação
     }
 
-    if (charsIn == "n") // Caso essa entrada seja o caractere b
+    if (serialInput == "n") // Caso essa entrada seja o caractere b
     {
-      interagir_buzzer_comando(texto[6], HIGH); // Vai ativar o buzzer por 2s e enviar a mensagem de confirmação
+      controlBuzzerCommand(messages[6], HIGH); // Vai ativar o buzzer por 2s e enviar a mensagem de confirmação
     }
 
-    if (charsIn == "m") // Caso essa entrada seja o caractere n
+    if (serialInput == "m") // Caso essa entrada seja o caractere n
     {
-      interagir_buzzer_comando(texto[7], LOW); // Vai desativar o buzzer e enviar a mensagem de confirmação
+      controlBuzzerCommand(messages[7], LOW); // Vai desativar o buzzer e enviar a mensagem de confirmação
     }
 
-    if (charsIn == "z") // Caso essa entrada seja o caractere z
+    if (serialInput == "z") // Caso essa entrada seja o caractere z
     {
       Serial.println("2"); // Ele vai mandar o ID 2 para o computador, onde será executada a lista de eventos
     }
 
-    if (charsIn == "c") // Caso essa entrada seja o caractere c
+    if (serialInput == "c") // Caso essa entrada seja o caractere c
     {
       Serial.println("3"); // Ele vai mandar o ID 3 para o computador, onde será exibido os controles de comando
     }
 
-    charsIn.toCharArray(printout, 21); // Ainda não sei para que isso serve
-    charsIn = "";                      // Define o caractere como nulo
+    serialInput.toCharArray(printout, 21); // Ainda não sei para que isso serve
+    serialInput = "";                      // Define o caractere como nulo
   }
 }
